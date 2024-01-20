90f0ff5c51d403224b4766e909536404c03018e3
Mercer-frey <deadharmony6969@gmail.com>
2024-01-20 19:34:53 +0200

    Bottombar done

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,10 +1,43 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+import {SIDEBAR_LINKS} from "@/constants";
+
 function Page() {
+	const pathname = usePathname();
 	
 	return (
-		<section>
-			Bottombar
+		<section className='bottombar'>
+			<div className='bottombar_container'>
+				{SIDEBAR_LINKS.map((link) => {
+					const isActive =
+						(pathname.includes(link.route) && link.route.length > 1) ||
+						pathname === link.route;
+					
+					return (
+						<Link
+							href={link.route}
+							key={link.label}
+							className={`bottombar_link ${isActive && "bg-primary-500"}`}
+						>
+							<Image
+								src={link.imgURL}
+								alt={link.label}
+								width={16}
+								height={16}
+								className='object-contain'
+							/>
+							
+							<p className='text-subtle-medium text-light-1 max-sm:hidden'>
+								{link.label.split(/\s+/)[0]}
+							</p>
+						</Link>
+					);
+				})}
+			</div>
 		</section>
 	);
 }
