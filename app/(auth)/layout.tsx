efdfada9ca65e9322e08729ca5b66cd53e4e655a
Mercer-frey <deadharmony6969@gmail.com>
2024-01-21 04:42:58 +0200

    onboarding prepared

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,13 +13,10 @@ export const metadata: Metadata = {
 function Header() {
 	return (
 		<header style={{display: "flex", justifyContent: "space-between", padding: 20}}>
-			<h1>My App</h1>
 			<SignedIn>
-				{/* Mount the UserButton component */}
 				<UserButton/>
 			</SignedIn>
 			<SignedOut>
-				{/* Signed out users get sign in button */}
 				<SignInButton/>
 			</SignedOut>
 		</header>
