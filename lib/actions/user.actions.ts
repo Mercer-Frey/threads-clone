4907bca0b517c45d54dfa7fec4197b58e0a2c58f
Mercer-frey <deadharmony6969@gmail.com>
2024-01-23 23:20:23 +0200

    added profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,8 @@
 import {revalidatePath} from "next/cache";
 
 import User from "@/lib/models/user.model";
+import Thread from "@/lib/models/thread.model";
+
 import {connectToDB} from "@/lib/mongoose";
 
 export async function updateUser({userId, bio, name, path, username, image}: IDTOUpdateUser): Promise<void> {
@@ -41,5 +43,37 @@ export async function fetchUser(userId: string) {
 		// });
 	} catch (error: any) {
 		throw new Error(`Failed to fetch user: ${error.message}`);
+	}
+}
+
+export async function fetchUserPosts(userId: string) {
+	try {
+		await connectToDB();
+		
+		// Find all threads authored by the user with the given userId
+		const threads = await User.findOne({id: userId}).populate({
+			path: "threads",
+			model: Thread,
+			populate: [
+				// {
+				// 	path: "community",
+				// 	model: Community,
+				// 	select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+				// },
+				{
+					path: "children",
+					model: Thread,
+					populate: {
+						path: "author",
+						model: User,
+						select: "name image id", // Select the "name" and "_id" fields from the "User" model
+					},
+				},
+			],
+		});
+		return threads;
+	} catch (error) {
+		console.error("Error fetching user threads:", error);
+		throw error;
 	}
 }
